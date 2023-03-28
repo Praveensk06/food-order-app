9c53b4904ddd76440fd55793fa3c79e95a0fd07a
Praveensk06 <113981543+Praveensk06@users.noreply.github.com>
2023-03-28 11:56:40 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import {
+  Header,
+  CreateContainer,
+  MainContainer,
+  HomeContainer,
+  MenuContainer,
+} from "./components";
+import { useStateValue } from "./context/StateProvider";
 
-function App() {
+import { getAllFoodItems } from "./utils/firebaseFunction";
+import { actionType } from "./context/Reducer";
+
+const App = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      <div className="width-screen h-auto flex flex-col bg-primary">
+        <Header />
+        {/* main container */}
+        <main className="mt-14 md:mt-20 px-4  md:px-16 py-4  w-full">
+          <Routes>
+            <Route path="/" element={<MainContainer />}></Route>
+            <Route path="/createItem" element={<CreateContainer />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
-}
+};
 
 export default App;
